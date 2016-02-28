ae4652daceaad4f7de2a65bafefa5376280d5fc9
David Gageot <david@gageot.net>
2016-02-28 22:09:41 +0100

    look
    Signed-off-by: David Gageot <david@gageot.net>

--- tables.js
+++ tables.js
@@ -4,7 +4,7 @@ var app = angular.module('myApp', []);
 
 app.controller('TablesCtrl', function ($scope) {
     $scope.reset = function () {
-        $scope.digit1 = Math.floor((Math.random() * 9) + 1);
+        $scope.digit1 = Math.floor((Math.random() * 6) + 1);
         $scope.digit2 = Math.floor((Math.random() * 9) + 1);
         $scope.answer = undefined;
     };
@@ -17,7 +17,7 @@ app.controller('TablesCtrl', function ($scope) {
         $scope.play()
     };
 
-    $scope.press = function($event) {
+    $scope.press = function ($event) {
         if ($event.keyCode == 13) {
             $scope.play()
         }
